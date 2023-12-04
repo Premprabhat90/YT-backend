342f404277b9cb00cf3bcccbf2201ab4a8bf6a2b
Prem Prabhat <120284744+Premprabhat90@users.noreply.github.com>
2023-12-05 01:32:54 +0530

    half express setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-
-// configDotenv .config({
-//     path:'./env'
-// })
-
+dotenv.config({
+    path: "./env",
+});
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`0 Server is running at port : 
+            ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGO db connection faild !!!", err);
+    });
